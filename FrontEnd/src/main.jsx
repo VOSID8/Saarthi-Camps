f6ab9aca77d6a246bc4c28c7757d108748c7cec8
HarshOnHub <harshpreetny@gmail.com>
2023-11-22 11:18:40 +0530

    added user roles - Admin and DEO

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthContextProvider } from './components/AuthProvider.jsx'
+import { AuthContextProvider } from './components/AuthProvider2.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- FrontEnd/src/pages/Details.jsx
+++ FrontEnd/src/pages/Details.jsx
@@ -5,16 +5,16 @@ import api_url from '../config';
 function RefugeeDetails() {
   const [refugeeId, setRefugeeId] = useState('');
   const [refugeeDetails, setRefugeeDetails] = useState(null);
-  const [medDetails, setMedDetails] = useState(null)
+  const [medDetails, setMedDetails] = useState(null);
 
   const handleSearch = () => {
-    const token = localStorage.getItem("accessToken")
+    const token = localStorage.getItem('accessToken');
 
     const config = {
       headers: {
-        "Authorization": `Bearer ${token}`
-      }
-    }
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
     axios.get(`${api_url}refugee/${refugeeId}`, config)
       .then(response => {
@@ -53,10 +53,13 @@ function RefugeeDetails() {
           <p>Gender: {refugeeDetails.gender}</p>
           <p>Date of Birth: {new Date(refugeeDetails.dob).toLocaleDateString()}</p>
           <p>Refugee ID: {refugeeDetails.refugeeId}</p>
-          {/* <p>Created At: {new Date(refugeeDetails.createdAt).toLocaleString()}</p> */}
-          {/* <p>Updated At: {new Date(refugeeDetails.updatedAt).toLocaleString()}</p> */}
+
           <h3>Medicine Details</h3>
-          <div>{medDetails[0].medicineName}</div>
+          {medDetails && medDetails.length > 0 ? (
+            <div>{medDetails[0].medicineName}</div>
+          ) : (
+            <div>No medicines ordered</div>
+          )}
         </div>
       )}
     </div>
